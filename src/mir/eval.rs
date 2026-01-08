1abb76c8ee4b4dfefd1462387dea51d45d959de6
Nils Martel <nilsmartel@yahoo.de>
2026-01-08 11:37:30 +0100

    Write perliminary logic to stringify instructions

--- src/mir/eval.rs
+++ src/mir/eval.rs
@@ -1,2 +1,11 @@
 use crate::mir::{CustomInstructionCode, Instruction};
 use crate::value::Value;
+fn string_concat(args: &[super::StaticExpression]) -> String {
+    let mut buffer = String::new();
+    for arg in args {
+        let value = evaluate(&arg.instr);
+        let str = value.to_string();
+        buffer.push_str(&str);
+    }
+    buffer
+}
