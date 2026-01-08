80f7ba5b179c7f4ef6649711dc3df483190a4a29
Nils Martel <nilsmartel@yahoo.de>
2026-01-08 11:37:44 +0100

    Write Evaluation for constant values

--- src/mir/eval.rs
+++ src/mir/eval.rs
@@ -1,5 +1,10 @@
 use crate::mir::{CustomInstructionCode, Instruction};
 use crate::value::Value;
+
+fn evaluate(op: &Instruction) -> Value {
+    match op {
+        Instruction::Const(v) => v.clone(),
+
 fn string_concat(args: &[super::StaticExpression]) -> String {
     let mut buffer = String::new();
     for arg in args {
