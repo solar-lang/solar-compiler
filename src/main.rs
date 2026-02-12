0c65c70502dded5b55d4df303a20f634f46f113a
Nils Martel <nilsmartel@yahoo.de>
2026-02-12 20:19:26 +0100

    Use better symbol names

--- src/main.rs
+++ src/main.rs
@@ -15,13 +15,13 @@ fn main() {
     // Root directory of the solar code project that we intent to compile
     let fsroot = std::env::args().nth(1).unwrap_or(".".to_string());
     // config file for solar code
-    let project_info = read_all_projects(&fsroot).expect("read in solar project and dependencies");
+    let project_info = read_all_projects(&fsroot).expect("read solar project and dependencies");
 
     let modules = read_modules(&project_info).expect("open and parse solar files");
 
     let ctx = CompilerContext::with_default_io(&project_info, modules);
 
-    let f_main = ctx.find_target_main().expect("find main function");
+    let main_symbol_id = ctx.find_target_main().expect("find main function");
 
     let function_id = ctx.compile_symbol(f_main, &[]).expect("compile code");
 
