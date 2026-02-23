f331e43bf49370a46f0b7c839be6bb9ba5d5819b
Nils Martel <nilsmartel@yahoo.de>
2026-02-23 10:30:13 +0100

    Include comment for testing purpose

--- samples/4/main.sol
+++ samples/4/main.sol
@@ -1,6 +1,17 @@
 use @std.types.int.Int
 use @std.io.println
 
+# open problem:
+# in what kind of package would the "toString" function live?
+# idea: in package of Type itself, and it is treated as typecast,
+# e.g. calling it "String".
+# That way it will get found automatically when resolving stuff.
+# next question:
+# Do I really want packages per directory?
+# alternative: packages per file.
+# possible problem: more subfolders
+# possible benefit: flat hierarchies. 
+# Though I think I actually _want_ to have int.add vs f32.add
 fun main() = two
 
 fun two() -> Int = 2
